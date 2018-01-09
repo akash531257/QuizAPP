bb6380cffb3cb902b349a11693a1e398aa5a0c7a
akash <akash531257@gmail.com>
2018-01-08 23:43:21 -0500

    lastt

---
+++ js/Quiz-Java-Fundamentals.js
@@ -0,0 +1,58 @@
+var init = { 
+     'questions': [ 
+		  {
+           "question": "What does SQL stand for?",
+           "answers": ["Strong Question Language", "Structured Question Language","Structured Query Language"],
+       	   "correctAnswer": 3
+       },
+       {
+           "question": "Which SQL statement is used to extract data from a database?",
+           "answers": ["SELECT", "EXTRACT","GET"],
+       	   "correctAnswer": 1
+       },
+       {
+           "question": "Which SQL statement is used to update data in a database?",
+           "answers": ["Modify", "Save as", "Update", "Save"],
+       	   "correctAnswer": 3
+       },
+       {
+           "question": "Which SQL statement is used to delete data from a database?",
+           "answers": ["Remove", "Delete", "Collapse"],
+       	   "correctAnswer": 2
+       },
+       {
+           "question": "Which SQL statement is used to insert new data in a database?",
+           "answers": ["Insert Into", "Add","Add new"],
+       	   "correctAnswer": 1
+       },
+       {
+           "question": "With SQL, how do you select a column named 'FirstName' from a table named 'Persons'?",
+           "answers": ["SELECT Persons.FirstName", "SELECT FirstName FROM Persons"],
+       	   "correctAnswer": 2
+       },
+     
+       {
+           'question': 'True or False: Java is a strongly typed language.',
+           'answers': ['True', 'False'],
+       	   'correctAnswer': 1
+       },
+       {
+           'question': 'In Java Strings are primitive types.',
+           'answers': ['True', 'False'],
+       	   'correctAnswer': 2
+       }
+
+     ],
+     
+	  'resultComments' :  
+	  {
+		   perfect: 'You\'re awesome enough to just walk into Mordor, aren&#39;t you?',
+			 excellent: 'My PRECIOUS!! You were outstanding!',
+			 good: 'Good enough, but beware the power of the ring.',
+			 average: 'An orc would have performed just as good as you!',
+			 bad: 'Is your mind poisoned by the ring?',
+			 poor: 'Is your mind poisoned by the ring?',
+			 worst: 'What is this new trolling?!'
+	  }
+
+ };
